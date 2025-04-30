af4f79b2ace6459ee45e6b246f9a490f3c3faf51
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-04-30 18:39:29 +0530

    new

--- src/models/plan.js
+++ src/models/plan.js
@@ -18,6 +18,10 @@ module.exports = (sequelize, DataTypes) => {
     plan_name: DataTypes.STRING,
     credit:DataTypes.INTEGER,
     price:DataTypes.FLOAT,
+    plan_key:DataTypes.STRING,
+    product_key:DataTypes.STRING,
+    plan_type:DataTypes.STRING,
+    plan_price:DataTypes.STRING,
     currency: {
       type: DataTypes.STRING,
       defaultValue: "usd",
